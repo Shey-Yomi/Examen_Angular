d53106245be8dd7a9356c2d821da8306acbf8bcd
shey-yomi-05 <sheylacorreo03@gmail.com>
2021-06-02 08:33:22 -0500

    Examen Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { CuerpoComponent} from './cuerpo/cuerpo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent, 
+    EncabezadoComponent,
+    CuerpoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
